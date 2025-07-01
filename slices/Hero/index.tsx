aafef916b15e89b85f20088e355e5fdaac10a5d7
waqarakrambhutta <waqarakrambhutta@gmail.com>
2025-07-01 10:01:42 +0500

    added Home slice

---
+++ slices/Hero/index.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import { Content } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage } from "@prismicio/next";
+
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+const Hero: FC<HeroProps> = ({ slice }) => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <PrismicRichText field={slice.primary.heading} />
+      <PrismicRichText field={slice.primary.body} />
+      <PrismicNextImage field={slice.primary.image} />
+    </section>
+  );
+};
+
+export default Hero;
